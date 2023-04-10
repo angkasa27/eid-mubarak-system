3d1595dbf61b7502d66fcdb3c22a76b41953bba6
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2023-04-10 12:18:55 +0700

    Update cors for origin

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://eid-mubarak-project.vercel.app",
   })
 );
 app.use("/", indexRouter);
