e624ffacc8b3ec5ef76bb35dd69df94bb2c0e5d7
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2023-04-20 13:19:18 +0700

    Fix comment auth

--- routes/comment.js
+++ routes/comment.js
@@ -1,10 +1,11 @@
 import express from "express";
 import CommentController from "../controllers/CommentController.js";
+import { jwtAuth } from "../middlewares/jwtAuth.js";
 
 var router = express.Router();
 
 router.get("/:username", [], CommentController.index);
 router.post("/:username", [], CommentController.store);
-router.put("/remove/:id", [], CommentController.hide);
+router.put("/remove/:id", [jwtAuth()], CommentController.hide);
 
 export default router;
